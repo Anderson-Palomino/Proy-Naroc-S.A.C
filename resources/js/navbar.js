dbb1343c6f349c58b8a71480aee2bc12b41913ad
Anderson-Palomino <u21203803@utp.edu.pe>
2024-12-04 19:18:30 -0500

    Base Barra de Navegacion

---
+++ resources/js/navbar.js
@@ -0,0 +1,8 @@
+function loadNavbar() {
+    fetch('/resources/html/navbar.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('navbar-placeholder').innerHTML = data;
+        });
+}
+window.onload = loadNavbar;
