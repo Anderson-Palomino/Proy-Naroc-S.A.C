c2c4afec01aefa4671ee5f5b1589d7a2e5e66f06
77ALDO77 <aldomoises14@gmail.com>
2024-12-10 10:34:18 -0500

    foter y logo

--- public/resources/js/footer.js
+++ public/resources/js/footer.js
@@ -3,8 +3,20 @@ function loadFooter() {
         .then(response => response.text())
         .then(data => {
             document.getElementById('footer-gaaa').innerHTML = data;
+            // Añadir el botón de WhatsApp después de cargar el footer
+            if (!document.querySelector('.whatsapp-float')) {
+                const whatsappButton = document.createElement('a');
+                whatsappButton.href = 'https://wa.me/51123456789';
+                whatsappButton.className = 'whatsapp-float';
+                whatsappButton.target = '_blank';
+                whatsappButton.innerHTML = '<i class="fab fa-whatsapp"></i>';
+                document.body.appendChild(whatsappButton);
+            }
         })
         .catch(error => console.error('Error loading footer:', error));
 }
+
+window.onload = loadFooter;
+
 
 window.onload = loadFooter;
